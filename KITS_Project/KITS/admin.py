7f85008ce2f34cce374dca952fa818915ba95584
dmiller80 <74140301+dmiller80@users.noreply.github.com>
2021-02-22 20:36:04 -0600

    Initial model
    With admin lists enabled

--- KITS_Project/KITS/admin.py
+++ KITS_Project/KITS/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Study, KitOrder
 
-# Register your models here.
+
+class KitOrderList(admin.ModelAdmin):
+    list_display = ( 'type', 'web_address', 'description' )
+    list_filter = ( 'type', 'web_address')
+    search_fields = ('type', )
+    ordering = ['web_address']
+
+
+class StudyList(admin.ModelAdmin):
+    list_display = ( 'IRB_number', 'pet_name', 'status' )
+    list_filter = ( 'IRB_number', 'start_date')
+    search_fields = ('IRB_number', )
+    ordering = ['IRB_number']
+
+
+admin.site.register(Study)
+admin.site.register(KitOrder)
